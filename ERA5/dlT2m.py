bf2997409f1ea86d62debcc937eec186d77d910b
François Massonnet <francois.massonnet@uclouvain.be>
2022-09-27 10:37:42 +0200

    download real time and netcdf

--- ERA5/dlT2m.py
+++ ERA5/dlT2m.py
@@ -5,50 +5,67 @@ import numpy as np
 c = cdsapi.Client()
 
 
+doDownload = False
+
 firstYear   = 1959  # in ERA5
 currentYear = date.today().year
+currentMonth= date.today().month
+currentDay  = date.today().day
 
-for year in np.arange(firstYear, currentYear):
-	print("Doing " + str(int(year)))
-	c.retrieve(
-	    'reanalysis-era5-single-levels',
-	    {
-		'product_type': 'reanalysis',
-		'format': 'grib',
-		'variable': '2m_temperature',
-		'year': str(int(year)),
-		'month': [
-		    '01', '02', '03',
-		    '04', '05', '06',
-		    '07', '08', '09',
-		    '10', '11', '12',
-		],
-		'day': [
-		    '01', '02', '03',
-		    '04', '05', '06',
-		    '07', '08', '09',
-		    '10', '11', '12',
-		    '13', '14', '15',
-		    '16', '17', '18',
-		    '19', '20', '21',
-		    '22', '23', '24',
-		    '25', '26', '27',
-		    '28', '29', '30',
-		    '31',
-		],
-		'time': [
-		    '00:00', '01:00', '02:00',
-		    '03:00', '04:00', '05:00',
-		    '06:00', '07:00', '08:00',
-		    '09:00', '10:00', '11:00',
-		    '12:00', '13:00', '14:00',
-		    '15:00', '16:00', '17:00',
-		    '18:00', '19:00', '20:00',
-		    '21:00', '22:00', '23:00',
-		],
-		'area': [
-		    50.8, 4.2, 50.7,
-		    4.3,
-		],
-	    },
-	    './data/download_T2M_' + str(year) + '.grib')
+lagDays = 7 # Nb days it take for ERA5 to deliver current day
+
+for year in np.arange(firstYear, currentYear + 1):
+	if year != currentYear:
+		listMonths = [str(m).zfill(2) for m in np.arange(1, 12 + 1)]
+		listDays   = [str(d).zfill(2) for d in np.arange(1, 31 + 1)]
+	else:
+		listMonths = [str(m).zfill(2) for m in np.arange(1, currentMonth + 1)]
+		listDays   = [str(d).zfill(2) for d in np.arange(1, currentDay + 1 - lagDays)]		
+	if (year != currentYear and doDownload) or (year == currentYear):
+		print("Doing " + str(int(year)))
+		c.retrieve(
+		    'reanalysis-era5-single-levels',
+		    {
+			'product_type': 'reanalysis',
+			'format': 'grib',
+			'variable': '2m_temperature',
+			'year': str(int(year)),
+			'month': listMonths, #[
+	#	    '01', '02', '03',
+	#		    '04', '05', '06',
+	#		    '07', '08', '09',
+	#		    '10', '11', '12',
+	#		],
+			'day': listDays,#[
+	#		    '01', '02', '03',
+	#		    '04', '05', '06',
+	#		    '07', '08', '09',
+	#		    '10', '11', '12',
+	#		    '13', '14', '15',
+	#		    '16', '17', '18',
+	#		    '19', '20', '21',
+	#		    '22', '23', '24',
+	#		    '25', '26', '27',
+	#		    '28', '29', '30',
+	#		    '31',
+	#		],
+			'time': [
+			    '00:00', '01:00', '02:00',
+			    '03:00', '04:00', '05:00',
+			    '06:00', '07:00', '08:00',
+			    '09:00', '10:00', '11:00',
+			    '12:00', '13:00', '14:00',
+			    '15:00', '16:00', '17:00',
+			    '18:00', '19:00', '20:00',
+			    '21:00', '22:00', '23:00',
+			],
+			'area': [
+			    50.8, 4.2, 50.7,
+			    4.3,
+			],
+			'format': 'netcdf',
+		    },
+		    './data/download_T2M_' + str(year) + '.nc')
+
+
+		
